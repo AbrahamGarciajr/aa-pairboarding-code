0743477544e6c3a4bc2d0a1be8bddd51f4ea3c6e
abraham <agarciasantos14@gmail.com>
2025-10-01 17:30:06 -0700

    length of longest substring

--- pairboarding.js
+++ pairboarding.js
@@ -890,33 +890,33 @@
 // There may exists other ways to achieve this answer too.
 
 
-const characterReplacement = function (s, k) {
-  let left = 0
-  let longest = 0
-  let freq = {}
-  let mostFreq = 0
+// const characterReplacement = function (s, k) {
+//   let left = 0
+//   let longest = 0
+//   let freq = {}
+//   let mostFreq = 0
 
-  for (let right = 0; right < s.length; right++) {
-    let char = s[right]
-    freq[char] = (freq[char] || 0) + 1
-    if (freq[char] > mostFreq) mostFreq = freq[char]
+//   for (let right = 0; right < s.length; right++) {
+//     let char = s[right]
+//     freq[char] = (freq[char] || 0) + 1
+//     if (freq[char] > mostFreq) mostFreq = freq[char]
 
 
-    if (((right - left + 1) - mostFreq > k)) {
-      freq[s[left]] -= 1
-      left++
-    }
+//     if (((right - left + 1) - mostFreq > k)) {
+//       freq[s[left]] -= 1
+//       left++
+//     }
 
-    if ((right - left + 1) > longest) {
-      longest = (right - left + 1)
-    }
+//     if ((right - left + 1) > longest) {
+//       longest = (right - left + 1)
+//     }
 
-  }
+//   }
 
-  return longest
-}
+//   return longest
+// }
 
-export default characterReplacement
+// export default characterReplacement
 
 
 // try {
@@ -928,10 +928,60 @@ export default characterReplacement
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// 3. Longest Substring Without Repeating Characters
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given a string s, find the length of the longest substring without duplicate characters.
 
 
 
+// Example 1:
 
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+// Constraints:
+
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+
+const lengthOfLongestSubstring = function (s) {
+  let freq = {}
+  let left = 0
+  let longest = 0
+
+  for (let right = 0; right < s.length; right++) {
+    let char = s[right]
+    freq[char] = (freq[char] || 0) + 1
+
+    while (freq[char] > 1) {
+      freq[s[left]] -= 1
+      left++
+    }
+
+    longest = Math.max(longest, right - left + 1)
+  }
+
+  return longest
+};
 
 
 
