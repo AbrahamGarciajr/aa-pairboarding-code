3153dcd03b31a153503596b40285f653ed8cfb01
abraham <agarciasantos14@gmail.com>
2025-10-04 21:31:53 -0700

    daily temps

--- test/week_5_problems.test.js
+++ test/week_5_problems.test.js
@@ -1,6 +1,6 @@
 import assert from "node:assert"
 
-import dailyTemperatures from '../week_5/week_5_problems'
+import dailyTemperatures from '../week_5/week_5_problems.js'
 
 
 describe('dailyTemperatures', function () {
@@ -13,17 +13,27 @@ describe('dailyTemperatures', function () {
     { args: [70, 70, 70, 70], expect: [0, 0, 0, 0] },
     { args: [60, 62, 61, 63, 58, 64], expect: [1, 2, 1, 2, 1, 0] },
     { args: [100], expect: [0] },
-    { args: [90, 85, 80, 70, 75, 78, 82, 60, 88], expect: [8, 7, 6, 1, 1, 1, 2, 1, 0] }]
+    { args: [90, 85, 80, 70, 75, 78, 82, 60, 88], expect: [0, 7, 4, 1, 1, 1, 2, 1, 0] }]
 
     tests.forEach(test => {
         let { args, expect } = test
-        if (expect === dailyTemperatures(args)) {
+
+        let res = dailyTemperatures(args)
+        let same = true
+        for (let i = 0; i < res.length; i++) {
+            if (res[i] !== expect[i]) {
+                same = false
+                break
+            }
+        }
+
+        if (same === true) {
             it(`should return ${expect} for ${args}`, function () {
-                assert.equal(dailyTemperatures(args), expect)
+                assert.deepStrictEqual(dailyTemperatures(args), expect)
             })
         } else {
             it(`returned ${dailyTemperatures(args)} instead of ${expect}`, function () {
-                assert.equal(dailyTemperatures(args), expect)
+                assert.deepStrictEqual(dailyTemperatures(args), expect)
             })
         }
 
--- week_5/week_5_problems.js
+++ week_5/week_5_problems.js
@@ -2,27 +2,30 @@
 
 
 
-export default dailyTemperatures = function (temps) {
+const  dailyTemperatures = function (temps) {
 
-    // let res = []
+    let res = []
 
-    // for (let i = 0; i < temps.length; i++) {
-    //     let fast = i + 1
-    //     while (temps[fast] <= temps[i] && fast < temps.length) {
-    //         fast++
-    //     }
-
-    //     temps[fast] > temps[i] ? res.push(fast - i) : res.push(0)
-    // }
-
-    // return res
-
-    let stack = []
-    let ph = 0
-
-    for (let i = 1; i < temps.length; i++) {
-        if (temps[i] > temps[ph]) {
-            stack
+    for (let i = 0; i < temps.length; i++) {
+        let fast = i + 1
+        while (temps[fast] <= temps[i] && fast < temps.length) {
+            fast++
         }
+
+        temps[fast] > temps[i] ? res.push(fast - i) : res.push(0)
     }
+
+    return res
+
+    // let stack = []
+    // let ph = 0
+
+    // for (let i = 1; i < temps.length; i++) {
+    //     if (temps[i] > temps[ph]) {
+    //         stack
+    //     }
+    // }
 };
+
+
+export default dailyTemperatures
