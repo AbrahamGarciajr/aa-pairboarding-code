4ecd2fe82966c4f41a7844f9eb07f821f6617a6e
abraham <agarciasantos14@gmail.com>
2025-10-06 22:20:14 -0700

    found a different way

--- week_5/week_5_problems.js
+++ week_5/week_5_problems.js
@@ -20,29 +20,49 @@
 
 // };
 
+// const dailyTemperatures = function (temps) {
+//     let res = new Array(temps.length).fill(0)
+
+//     let p1 = 0
+//     let p2 = p1 + 1
+
+//     while (p1 < temps.length) {
+//         if (temps[p2] > temps[p1]) {
+//             res[p1] = p2 - p1
+//             p1++
+//             p2 = p1 + 1
+//         } else if (p2 === temps.length) {
+//             p1++
+//             p2 = p1 + 1
+//         } else {
+//             p2++
+//         }
+//     }
+//     return res
+// }
+
 const dailyTemperatures = function (temps) {
-    let res = new Array(temps.length).fill(0)
+    let temps1 = [...temps]
+    let res = new Array(temps1.length).fill(0)
 
-    let p1 = 0
-    let p2 = p1 + 1
+    while (temps1.length) {
+        let i = temps1.length - 2
+        // console.log(temps1, the )
+        // console.log(res, 'the res')
+        let end = temps1.length - 1
+        let temp = temps1.pop()
 
-    while (p1 < temps.length) {
-        if (temps[p2] > temps[p1]) {
-            res[p1] = p2 - p1
-            p1++
-            p2 = p1 + 1
-        } else if (p2 === temps.length) {
-            p1++
-            p2 = p1 + 1
-        } else {
-            p2++
+        while (i >= 0) {
+            // console.log(temp, temps1[i])
+            if (temps1[i] < temp) {
+                res[i] = end - i
+            }
+            i--
         }
     }
+    // console.log(res)
     return res
 }
-
-console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
-
 
 // { args: [73, 74, 75, 71, 69, 72, 76, 73], expect: [1, 1, 4, 2, 1, 1, 0, 0] },
 //     { args: [30, 40, 50, 60], expect: [1, 1, 1, 0] },
